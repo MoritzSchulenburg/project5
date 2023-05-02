602f3aaf008a5814d7149bdbf2ee56383e7197dc
Moritz Schulenburg <moritzschulenburg@web.de>
2023-05-02 18:20:45 +0200

    data file edit and old code deleted

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,1 @@
-import List from "./list";
-
-export default function HomePage({ data, onToggleFavorite, artPiecesInfo }) {
-  return (
-    <List
-      data={data}
-      onToggleFavorite={onToggleFavorite}
-      artPiecesInfo={artPiecesInfo}
-    />
-  );
-}
+export default function HomePage({}) {}
