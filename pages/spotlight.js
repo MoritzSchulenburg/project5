2b4eadd3af9e24d211759362e030c1c68cb8c149
mgczanievicz <gimena@spiced-academy.com>
2023-05-03 14:26:28 +0200

    fixted spotlight

--- pages/spotlight.js
+++ pages/spotlight.js
@@ -2,29 +2,19 @@ import { useState } from "react";
 import Spotlight from "../components/Spotlight";
 import { useEffect } from "react";
 
-// console.log(getRandomInt(0, 11));
-
 export default function SpotlightPage({ pieces }) {
-  const [count, setCount] = useState(getRandomInt(0, pieces.length));
-  const [art, setArt] = useState(pieces[count]);
+  const [art, setArt] = useState({});
 
-  // // const count = getRandomInt(0, 11);
+  useEffect(() => {
+    function getRandomInt(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
 
-  // useEffect(() => {
-  //   setPiece(pieces[count]);
-  // }, []);
-
-  console.log("pieces: ", pieces);
-  console.log("piece: ", art);
-  console.log("coutn: ", count);
-  console.log("pieces.lenght", pieces.length);
-
-  function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-
-    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
-  }
+      // The maximum is exclusive and the minimum is inclusive
+      return Math.floor(Math.random() * (max - min) + min);
+    }
+    setArt(pieces[getRandomInt(0, pieces.length)]);
+  }, [pieces]);
 
   return (
     <>
