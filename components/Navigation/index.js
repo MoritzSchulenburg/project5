35264ce6a79a9e0e39c3f351e2f32202284812f6
Moritz Schulenburg <moritzschulenburg@web.de>
2023-05-03 17:07:17 +0200

    changes

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -32,7 +32,9 @@ const StyledNavigation = styled.div`
   bottom: 0;
   width: 100%;
   border-radius: 5px;
-  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.2);
+  //   box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.2);
+  box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
+    rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
 `;
 
 const StyledLi = styled.li`
